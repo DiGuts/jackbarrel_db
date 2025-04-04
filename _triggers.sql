ffa5b68c522617757cc8bdeb3de8f80058332b39
jannoguer <jannoguer@gmail.com>
2025-04-04 16:35:33 +0200

    restock reformulat

--- _triggers.sql
+++ _triggers.sql
@@ -5,42 +5,23 @@ CREATE OR REPLACE TRIGGER restock
 DECLARE
     p                 producte%rowtype;
     codi_comanda      INT;
-    comanda_quantitat NUMBER;
+    comanda_quantitat NUMBER := 0;
 BEGIN
     SELECT * INTO p FROM producte WHERE codiproducte = :new.producte;
 
     SELECT NVL(MAX(codicomanda), 0) INTO codi_comanda FROM comanda;
     codi_comanda := codi_comanda + 1;
 
-    UPDATE PRODUCTE SET STOCKACTUAL = STOCKACTUAL - quantitat
-    WHERE CODIPRODUCTE = cproducte;
-
     IF p.stockactual < p.stockminim THEN
         comanda_quantitat := p.stockminim - p.stockactual;
+
         INSERT INTO comanda
-        VALUES (codi_comanda, p.codiproducte, comanda_quantitat, SYSDATE);
-        ACTUALITZAR_COMPRES_PROVEIDOR(p.codiproducte, comanda_quantitat);
+        VALUES (codi_comanda, p.codiproducte, comanda_quantitat, SYSDATE, 0);
+        actualitzar_compres_proveidor(p.codiproducte, comanda_quantitat);
     END IF;
 EXCEPTION
     WHEN OTHERS THEN
         dbms_output.put_line(SQLERRM);
-END;
-
-CREATE OR REPLACE TRIGGER vendadetall_validacio
-    BEFORE UPDATE OR INSERT
-    ON vendadetall
-    FOR EACH ROW
-DECLARE
-    producte_id    NUMBER;
-    producte_stock NUMBER;
-BEGIN
-    SELECT codiproducte, stockactual INTO producte_id, producte_stock FROM producte WHERE :NEW.producte = codiproducte;
-
-    IF :NEW.quantitat < producte_stock THEN
-        UPDATE producte
-        SET stockactual = PRODUCTE.stockactual - :NEW.quantitat
-        WHERE codiproducte = producte_id;
-    END IF;
 END;
 
 CREATE OR REPLACE TRIGGER evitar_venda_fora_horari
